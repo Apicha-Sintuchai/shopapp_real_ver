e3b61677385ed4ad7bc727fbdd5d959c791008c9
Apicha sintuchai <apichai2556anet@gmail.com>
2025-01-04 21:40:31 +0700

    move nextjs to vue

---
+++ frontend/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>dawdawdawdawdawdawd</h1>
+  <RouterView />
+</template>
+
+<script>
+import { RouterView } from 'vue-router';
+
+
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped></style>
